faf334b7c1ca7dca496ee3323825be9be295f950
Rahwa Yohannes <rahwahaile578@gmail.com>
2022-12-17 12:57:32 +0300

    filtering functionality

--- controllers/products.js
+++ controllers/products.js
@@ -3,11 +3,29 @@ require('express-async-errors')
 
 const getAllProductsStatic = async (req, res)=>{
     // throw new Error('Testing async error')
-    const products = await Product.find()
-    res.status(200).json({ products })
+    const search = 'ab'
+    const products = await Product.find({name: {$regex: search, $options: 'i'}})
+    
+    res.status(200).json({ products, noOfhits: products.length })
 }
-const getAllProducts = (req, res)=>{
-    res.status(200).json({ msg: 'Products route'})
+const getAllProducts = async (req, res)=>{
+    const {featured, company, name} = req.query
+    const queryObject = {}
+
+    if(featured){
+        // queryObject.featured = featured
+        queryObject.featured = featured==='true' ? true : false
+    }
+    if(company){
+        queryObject.company = company
+    }
+    if(name){
+        queryObject.name = { $regex: name, $options: 'i'}
+    }
+    console.log(queryObject)
+
+    const products = await Product.find(queryObject)
+    res.status(200).json({ products, noOfhits: products.length })
 }
 
 module.exports = { getAllProductsStatic, getAllProducts }
