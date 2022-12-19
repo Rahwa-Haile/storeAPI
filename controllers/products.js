8dcae552ec84ca0ddc0b5748e2b68473f020b23b
Rahwa Yohannes <rahwahaile578@gmail.com>
2022-12-19 15:44:33 +0300

    Pagination functionality using limit and skip

--- controllers/products.js
+++ controllers/products.js
@@ -5,11 +5,15 @@ const getAllProductsStatic = async (req, res)=>{
     // throw new Error('Testing async error')
     const search = 'ab'
     // const products = await Product.find({name: {$regex: search, $options: 'i'}})
-    const products = await Product.find().sort('-name -price')
+    const products = await Product.find()
+        .sort('name -price')
+        .select('name price createdAt')
+        .limit(10)
+        .skip(5)
     res.status(200).json({ products, noOfhits: products.length })
 }
 const getAllProducts = async (req, res)=>{
-    const {featured, company, name, sort} = req.query
+    const {featured, company, name, sort, fields} = req.query
     const queryObject = {}
 
     if(featured){
@@ -33,7 +37,16 @@ const getAllProducts = async (req, res)=>{
     else{
         result = result.sort('createdAt')
     }
-    const products = await result
+    if(fields){
+        const selectedFields = fields.split(',').join(' ')
+        result = result.select(selectedFields)
+    }
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page-1) * limit
+
+    const products = await result.limit(limit).skip(skip)
     res.status(200).json({ products, noOfhits: products.length })
 }
 
--- models/products.js
+++ models/products.js
@@ -19,7 +19,7 @@ const productSchema = new mongoose.Schema({
     },
     createdAt: {
         type: Date,
-        dafault: Date.now()
+        default: Date.now()
     },
     company: {
         type: String,
